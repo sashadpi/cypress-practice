89e17bf2138a3c15cc7f34fa69e521aa2ff59386
*Sasha* <*sashadpi68@gmail.com*>
2026-02-13 09:08:43 +0200

    chore: initialize project with package.json and test.js files
    - Added package.json with project metadata, dependencies, and scripts.
    - Created an empty test.js file for Cypress tests.

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: "lesson-18/cy-tests/**/*.cy.{js,jsx,ts,tsx}",
+    video: false,
+    screenshotOnRunFailure: true,
+  },
+});
